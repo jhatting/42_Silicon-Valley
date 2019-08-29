3b0ca90d95ef5e0b2263e6b639206a1b6121dba3
Scrumpy Hat <shat@e1z1r7p30.42.us.org>
2019-08-28 22:41:32 -0700

    libft no notes must revise

---
+++ libft/strmapi.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strmapi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shat <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/28 22:34:06 by shat              #+#    #+#             */
+/*   Updated: 2019/08/28 22:34:13 by shat             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*new;
+	unsigned int	i;
+
+	if (s == NULL || f == NULL || !(new = (char *)malloc(sizeof(char) \
+	* (ft_strlen(s) + 1))))
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		new[i] = f(i, s[i]);
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
+}
