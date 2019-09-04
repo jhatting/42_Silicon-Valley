0f32707cad576b9a30db64f3ae174bb8f32f2c2f
Scrumpy Hat <shat@e1z3r7p9.42.us.org>
2019-09-03 18:12:17 -0700

    libft add-on

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: shat <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/03 17:07:42 by shat              #+#    #+#             */
-/*   Updated: 2019/09/03 17:27:05 by shat             ###   ########.fr       */
+/*   Updated: 2019/09/03 18:10:19 by shat             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,6 +79,7 @@ void	ft_putchar(char c);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putendl(char const *s);
 void	ft_putendl_fd(char const *s, int fd);
+void	ft_putnbr(int nbr);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_putstr(const char *s);
 void	ft_putstr_fd(char const *s, int fd);
